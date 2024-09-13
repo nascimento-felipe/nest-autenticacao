36558410ecb781bf35b090880aa31e1ba3bf1fa5
Felipe <61094586+nascimento-felipe@users.noreply.github.com>
2024-09-12 23:33:14 -0300

    ✨ feat(app): #4 Adicionado local strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-  validateUser(email: string, password: string) {
-    throw new Error('Method not implemented.');
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    // dá pra tirar todos esses ifs aqui usando
+    // early return, mas vou deixar assim por enquanto
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+
+    throw new Error('Email address or password provided is incorrect');
   }
 }
