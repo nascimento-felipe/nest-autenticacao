b5f3c13a67c08b2fac5a203037f5336dc2df8bc4
Felipe <61094586+nascimento-felipe@users.noreply.github.com>
2024-09-12 23:57:38 -0300

    ✨ feat(app): #4 Adicionado a logica de gerar o jwt ao logar
    Foi adicionada a funcao que gera o jwt caso as credenciais de login sejam os mesmo do banco de dados
    
    Closes: #4

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
+import { UserPayload } from './models/UserPayload';
+import { UserToken } from './models/UserToken';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
@@ -23,5 +30,19 @@ export class AuthService {
     }
 
     throw new Error('Email address or password provided is incorrect');
+  }
+
+  login(user: User): UserToken {
+    const payload: UserPayload = {
+      sub: user.id,
+      email: user.email,
+      name: user.name,
+    };
+
+    const jwtToken = this.jwtService.sign(payload);
+
+    return {
+      access_token: jwtToken,
+    };
   }
 }
